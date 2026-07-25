07d60af80c37c057669e6d85d265f5deae6696a0
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-07-25 23:05:53 +0530

    resources

---
+++ lib/xero/accounting/resources.ex
@@ -0,0 +1,14 @@
+# This file re-exports all the accounting sub-modules for convenience.
+# Each module is defined in its own file; this is kept for documentation.
+# See:
+#   lib/xero/accounting/invoices.ex
+#   lib/xero/accounting/contacts.ex
+#   lib/xero/accounting/accounts.ex   (also contains BankTransactions, BankTransfers,
+#                                       Payments, BatchPayments, CreditNotes, Overpayments,
+#                                       Prepayments, PurchaseOrders, Quotes, Items,
+#                                       TaxRates, TrackingCategories, ContactGroups,
+#                                       Currencies, RepeatingInvoices, ManualJournals,
+#                                       LinkedTransactions, Journals, Organisation,
+#                                       Users, BrandingThemes, Budgets, InvoiceReminders,
+#                                       PaymentServices, History)
+#   lib/xero/accounting/reports.ex
